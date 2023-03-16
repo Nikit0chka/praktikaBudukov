cca4d1f762926f5fa5f25df4d70dceba262825ff
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-03-16 04:27:41 +0400

    1

--- Sir/Praktik1/EmployeeRegistrationWindow.xaml.cs
+++ Sir/Praktik1/EmployeeRegistrationWindow.xaml.cs
@@ -1,27 +1,32 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 using System.Windows;
-using System.Windows.Media;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Praktik1;
 
 public partial class EmployeeRegistrationWindow : Window
 {
-    string _directoryPath = @"C:\Users\voron\OneDrive\Рабочий стол\BudukovPraktika\FinalDirectory";
-    string _filePath = @"C:\Users\voron\OneDrive\Рабочий стол\BudukovPraktika\FinalDirectory\employee.txt";
+    private string _directoryPath = @"C:\Users\voron\OneDrive\Рабочий стол\BudukovPraktika\FinalDirectory";
+    private string _filePath = @"C:\Users\voron\OneDrive\Рабочий стол\BudukovPraktika\FinalDirectory\employee.txt";
+    private bool _isAllInputCorrect = false;
+    private ValidateEmployeeRegistrationTextBoxes validateTextBoxes;
 
     public EmployeeRegistrationWindow()
     {
         WindowStartupLocation = WindowStartupLocation.CenterScreen;
         InitializeComponent();
+
+        validateTextBoxes = new ValidateEmployeeRegistrationTextBoxes();
+        DataContext = validateTextBoxes;
     }
 
     private void registerUser_Click(object sender, RoutedEventArgs e)
     {
-        if (CheckAllINput())
+        if (_isAllInputCorrect)
             FillFile();
+
     }
 
     private void backButton_Click(object sender, RoutedEventArgs e) => OpenAuthorizationWindow();
@@ -31,73 +36,6 @@ public partial class EmployeeRegistrationWindow : Window
         AuthorizationWindow authorizationWindow = new AuthorizationWindow();
         authorizationWindow.Show();
         Close();
-    }
-
-    private bool CheckAllINput()
-    {
-        bool isNoException = true;
-        string exceptionsContent = "";
-
-        if (!CheckIDInput())
-        {
-            idTextBox.BorderBrush = Brushes.Red;
-            isNoException = false;
-            exceptionsContent += "Check ID input!\n";
-        }
-        else
-            idTextBox.BorderBrush = Brushes.Green;
-        if (!CheckEmailInput())
-        {
-            emailTextBox.BorderBrush = Brushes.Red;
-            isNoException = false;
-            exceptionsContent += "Check email input!\n";
-        }
-        else
-            emailTextBox.BorderBrush = Brushes.Green;
-        if (!CheckPhoneNumberInput())
-        {
-            phoneNumberTextBox.BorderBrush = Brushes.Red;
-            isNoException = false;
-            exceptionsContent += "Check phone number input!\n";
-        }
-        else
-            phoneNumberTextBox.BorderBrush = Brushes.Green;
-        if (!CheckNameSurnamePatronymicInput(nameTextBox))
-        {
-            nameTextBox.BorderBrush = Brushes.Red;
-            isNoException = false;
-            exceptionsContent += "Check name input!\n";
-        }
-        else
-            nameTextBox.BorderBrush = Brushes.Green;
-        if (!CheckNameSurnamePatronymicInput(surnameTextBox))
-        {
-            surnameTextBox.BorderBrush = Brushes.Red;
-            isNoException = false;
-            exceptionsContent += "Check surname input!\n";
-        }
-        else
-            surnameTextBox.BorderBrush = Brushes.Green;
-        if (!CheckNameSurnamePatronymicInput(patronymicTextBox))
-        {
-            patronymicTextBox.BorderBrush = Brushes.Red;
-            isNoException = false;
-            exceptionsContent += "Check patronymic input!\n";
-        }
-        else
-            patronymicTextBox.BorderBrush = Brushes.Green;
-        if (!CheckPassportInput())
-        {
-            passportTextBox.BorderBrush = Brushes.Red;
-            isNoException = false;
-            exceptionsContent += "Check passport input!\n";
-        }
-        else
-            passportTextBox.BorderBrush = Brushes.Green;
-
-        MessageBox.Show(exceptionsContent == "" ? "Registred sucsessfull!" : exceptionsContent);
-
-        return isNoException;
     }
 
     private void FillFile()
@@ -149,45 +87,13 @@ public partial class EmployeeRegistrationWindow : Window
         return true;
     }
 
-    private bool CheckNameSurnamePatronymicInput(TextBox input)
+    private void InputError(object sender, ValidationErrorEventArgs e)
     {
-        if (input.Text.Length == 0 && input.Name == "patronymicTextBox")
-            return true;
-        if (input.Text.Length == 0)
-            return false;
-
-        return (input.Text.Any(Char.IsLetter) &&
-            input.Text.Skip(1).Any(Char.IsLower) &&
-            !Char.IsLower(input.Text[0])) ? true : false;
+        _isAllInputCorrect = false;
     }
 
-    private bool CheckIDInput()
+    private void AnyTextChange(object sender, TextChangedEventArgs e)
     {
-        return (idTextBox.Text.Length != 0 &&
-            idTextBox.Text.Any(Char.IsNumber)) ? true : false;
-    }
-
-    private bool CheckPassportInput()
-    {
-        return (passportTextBox.Text.Length == 10 &&
-            passportTextBox.Text.Any(Char.IsNumber)) ? true : false;
-    }
-
-    private bool CheckPhoneNumberInput()
-    {
-        if (phoneNumberTextBox.Text.Length == 0)
-            return true;
-        return ((phoneNumberTextBox.Text[0] == '+' && phoneNumberTextBox.Text.Length == 12) ||
-            (phoneNumberTextBox.Text[0] == '8' && phoneNumberTextBox.Text.Length == 11) &&
-            phoneNumberTextBox.Text.Skip(1).Any(Char.IsNumber)) ? true : false;
-    }
-
-    private bool CheckEmailInput()
-    {
-        return (emailTextBox.Text.Length != 0 &&
-            emailTextBox.Text.Contains('@') &&
-            emailTextBox.Text.Substring
-            (emailTextBox.Text.IndexOf('@')).
-            Contains('.')) ? true : false;
+        _isAllInputCorrect = true;
     }
 }
---
+++ Sir/Praktik1/ValidateEmployeeRegistrationTextBoxes.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Linq;
+using System.ComponentModel;
+
+namespace Praktik1;
+
+internal class ValidateEmployeeRegistrationTextBoxes : IDataErrorInfo
+{
+    public string? ID { get; set; }
+    public string? Surname { get; set; }
+    public string? Name { get; set; }
+    public string? Patronymic { get; set; }
+    public string? Passport { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Email { get; set; }
+
+    private bool IsNoErrors;
+
+    public bool IsAllInputCorrect()
+    {
+        return IsNoErrors;
+    }
+
+    public string this[string columnName]
+    {
+        get
+        {
+            string error = string.Empty;
+            switch (columnName)
+            {
+                case "ID":
+                    if (ID == null)
+                    {
+                        error = "ID must be not empty!";
+                        break;
+                    }
+                    if (!ID.Any(Char.IsNumber))
+                        error = "ID must contain only numbers!";
+                    break;
+
+                case "Surname":
+                    if (Surname == null)
+                    {
+                        error = "Surname must be not empty!";
+                        break;
+                    }
+                    if (!Surname.Any(Char.IsLetter))
+                        error = "Surname can contain only letters!";
+                    if (!Surname.Skip(1).Any(Char.IsLower))
+                        error = "All surname letters must be lower except first!";
+                    if (Char.IsLower(Surname[0]))
+                        error = "First surname letter must be higher!";
+                    break;
+
+                case "Name":
+                    if (Name == null)
+                    {
+                        error = "Name must be not empty!";
+                        break;
+                    }
+                    if (!Name.Any(Char.IsLetter))
+                        error = "Name can contain only letters!";
+                    if (!Name.Skip(1).Any(Char.IsLower))
+                        error = "All name letters must be lower except first!";
+                    if (Char.IsLower(Name[0]))
+                        error = "First name letter must be higher!";
+                    break;
+
+                case "Patronymic":
+                    if (Patronymic == null)
+                        break;
+                    if (!Patronymic.Any(Char.IsLetter))
+                        error = "Patronymic can contain only letters!";
+                    if (!Patronymic.Skip(1).Any(Char.IsLower))
+                        error = "All patronymic letters must be lower except first!";
+                    if (Char.IsLower(Patronymic[0]))
+                        error = "First patronymic letter must be higher!";
+                    break;
+
+                case "Passport":
+                    if (Passport == null)
+                    {
+                        error = "Passport must be not empty!";
+                        break;
+                    }
+                    if (Passport.Length != 10)
+                        error = "Passport must include ten numbers!";
+                    if (!Passport.Any(Char.IsNumber))
+                        error = "Passport must include only numbers!";
+                    break;
+
+                case "PhoneNumber":
+                    if (PhoneNumber == null)
+                        break;
+                    if ((PhoneNumber[0] != '+' || PhoneNumber.Length != 12) && (PhoneNumber[0] != '8' || PhoneNumber.Length != 11))
+                        error = "Phone number must look like +79873691806 or 89873691806!";
+                    if (!PhoneNumber.Skip(1).Any(Char.IsNumber))
+                        error = "Phone number must contain only numbers except '+'!";
+                    break;
+
+                case "Email":
+                    if (Email == null)
+                    {
+                        error = "Email must be not empty!";
+                        break;
+                    }
+                    if (!Email.Contains('@') || !Email.Substring(Email.IndexOf('@')).Contains('.'))
+                        error = "Email must contain '@' and '.' in domen!";
+                    break;
+            }
+            IsNoErrors = (error == string.Empty) ? true : false;
+
+            return error;
+        }
+    }
+    public string Error
+    {
+        get => throw new NotImplementedException();
+    }
+}
